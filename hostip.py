250c7c728fb3ba6d2ea644496ac823e7fc0e4423
Jason Mann <aztuxmann+github@gmail.com>
2016-02-10 17:39:53 -0700

    Correcting IPv6 failures
    I have updated the code because hostname -I caused the script to fail if non-integers were detected in the line. The new way is more robust.

--- hostip.py
+++ hostip.py
@@ -24,36 +24,39 @@
 import os
 import time
 import re
-from subprocess import *
 import numpy as np
 import smbus
+import commands
 
-cmd = "hostname -I"		# 	Linux command to get IP addres
+
 i2caddr = 0x5A			# 	Address must correspond with shop_ip.c's address #7 bit address (will be left shifted to add the read write bit)
 refresh = 120			#	Sets the time between sending data to the LED display
 
 global bus
-### 	I think I can send this to the ATMEL directly	###
-def ip_to_dword(cmd):
-	# Changes the IP addr to a 32-bit integer.
-	p = Popen(cmd, shell=True, stdout=PIPE)
-	st = p.communicate()[0]
-	st = st.split(".")
-	if "\n" in st:
-		# Let the user know that we don't see an IP address
+
+# Changes the IP addr to a 32-bit integer.
+def ip_to_dword():
+
+	st = commands.getoutput("/sbin/ifconfig | grep Bcast")
+
+	if not st: 	# Let the user know that we don't see an IP address
 		time.sleep (5)
 		return int(4278255360)	# Returns 255.0.255.0
-	return int("%02x%02x%02x%02x" % (int(st[0]),int(st[1]),int(st[2]),int(st[3])),16)
+	
+	else:		# Let the user know that we see an IP address
+		# The line below takes the output of the terminal command, segregates the 
+		# IP address, and then removes the dots to create an array.
+		st = commands.getoutput("/sbin/ifconfig | grep Bcast").split()[1][5:].split(".")
+		return int("%02x%02x%02x%02x" % (int(st[0]),int(st[1]),int(st[2]),int(st[3])),16)
 
 while True:
-	# Change the IP addr to a 32-bit integer.
 	bus = smbus.SMBus(1)	# 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
-	dword_integer = ip_to_dword(cmd)
+	dword_integer = ip_to_dword()
 	
 	# Separate the 32-bit integer into 8 bit integers.
-	# Also use np.int to change type of data to an integer to send.
-	MSO = np.int(dword_integer / 16777216)						#	Most Significant Octet
-	SMSO = np.int((dword_integer % 16777216) / 65536)			#	Second Most Significant Octet
+	# Also use np.int to change type of data to an integer before sending.
+	MSO = np.int(dword_integer / 16777216)				#	Most Significant Octet
+	SMSO = np.int((dword_integer % 16777216) / 65536)		#	Second Most Significant Octet
 	SLSO = np.int(((dword_integer % 16777216) % 65536) / 256)	#	Second Most Significant Octect
 	LSO = np.int(((dword_integer % 16777216) % 65536) % 256)	#	Least Significant Octet
 		
@@ -63,9 +66,9 @@ while True:
 	# print TMSO
 	# print LMSO
 	
-###		This portion takes the IP address and sends it to the add-on board 		###
+###	This portion takes the IP address and sends it to the add-on board	###
 	
-	# This is kind of rube goldergish. 
+	# This is kind of rube goldberg-ish because the MSO in the "char cmd" or register parameter.
 	ip_values = [SMSO, SLSO, LSO]
 	
 	bus.write_i2c_block_data(i2caddr, MSO, ip_values)
